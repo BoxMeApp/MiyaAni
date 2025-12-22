9b56bf147bff1652cc70f24fec13a1a5dbab7cf0
Dz <MathBeWithCode@gmail.com>
2025-12-22 21:19:56 +0800

    deps

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:miya_ani/l10n/generated/localizations.dart';
 import 'package:subject_repository/subject_repository.dart';
+import 'package:infix/via.dart';
 
 import 'screens/router.dart';
 
@@ -14,14 +15,19 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiRepositoryProvider(
-      providers: [RepositoryProvider(create: (context) => SubjectRepository())],
-      child: MaterialApp.router(
-        routerConfig: router,
-        onGenerateTitle: (context) => AppLocalizations.of(context)!.appTitle,
-        localizationsDelegates: AppLocalizations.localizationsDelegates,
-        supportedLocales: AppLocalizations.supportedLocales,
-      ),
-    );
+    return via(
+          (Widget c) => MultiRepositoryProvider(
+            providers: [
+              RepositoryProvider(create: (context) => SubjectRepository()),
+            ],
+            child: c,
+          ),
+        ) >
+        MaterialApp.router(
+          routerConfig: router,
+          onGenerateTitle: (context) => AppLocalizations.of(context)!.appTitle,
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          supportedLocales: AppLocalizations.supportedLocales,
+        );
   }
 }
