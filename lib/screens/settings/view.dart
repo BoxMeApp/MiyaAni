21995fe9f8c1cdce629ba74b9de7600d08f87d1f
worranhin <worranhin@foxmail.com>
2025-12-24 23:01:29 +0800

    modify settingsPage UI

--- lib/screens/settings/view.dart
+++ lib/screens/settings/view.dart
@@ -12,19 +12,39 @@ class SettingsPage extends StatelessWidget {
     final l10n = AppLocalizations.of(context)!;
     return Scaffold(
       appBar: AppBar(title: const Text('Settings')),
-      body: Center(
-        child: StreamBuilder(
-          stream: prefs.locale$,
-          initialData: prefs.locale,
-          builder: (context, snapshot) => DropdownButton<Locale?>(
-            value: snapshot.data,
-            onChanged: (locale) => prefs.locale = locale,
-            items: [
-              .new(value: null, child: Text(l10n.systemDefault)),
-              .new(value: Locale('en'), child: Text(l10n.en)),
-              .new(value: Locale('zh'), child: Text(l10n.zh)),
-            ],
-          ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: .start,
+          children: [
+            Text("Language", style: Theme.of(context).textTheme.titleMedium),  // TODO: l10n.language
+            RadioGroup<Locale?>(
+              onChanged: (locale) => prefs.locale = locale,
+              groupValue: prefs.locale,
+              child: Column(
+                children: [
+                  RadioListTile<Locale?>(value: null, title: Text(l10n.systemDefault)),  // TODO: 若系统默认与所选的语言相同，单选框并不改变
+                  RadioListTile<Locale?>(value: Locale('en'), title: Text(l10n.en)),
+                  RadioListTile<Locale?>(value: Locale('zh'), title: Text(l10n.zh)),
+                ],
+              ),
+            ),
+            // StreamBuilder(
+            //   stream: prefs.locale$,
+            //   initialData: prefs.locale,
+            //   builder: (context, snapshot) => ListTile(
+            //     leading: DropdownButton<Locale?>(
+            //       value: snapshot.data,
+            //       onChanged: (locale) => prefs.locale = locale,
+            //       items: [
+            //         .new(value: null, child: Text(l10n.systemDefault)),
+            //         .new(value: Locale('en'), child: Text(l10n.en)),
+            //         .new(value: Locale('zh'), child: Text(l10n.zh)),
+            //       ],
+            //     ),
+            //   ),
+            // ),
+          ],
         ),
       ),
     );
