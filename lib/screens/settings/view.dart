e05b47067126a30bf4752ffa74ae81d5da1d85e8
worranhin <worranhin@foxmail.com>
2025-12-25 19:37:03 +0800

    fix: 修复若系统默认与所选的语言相同，单选框并不改变的 bug

--- lib/screens/settings/view.dart
+++ lib/screens/settings/view.dart
@@ -17,33 +17,35 @@ class SettingsPage extends StatelessWidget {
         child: Column(
           crossAxisAlignment: .start,
           children: [
-            Text("Language", style: Theme.of(context).textTheme.titleMedium),  // TODO: l10n.language
-            RadioGroup<Locale?>(
-              onChanged: (locale) => prefs.locale = locale,
-              groupValue: prefs.locale,
-              child: Column(
-                children: [
-                  RadioListTile<Locale?>(value: null, title: Text(l10n.systemDefault)),  // TODO: 若系统默认与所选的语言相同，单选框并不改变
-                  RadioListTile<Locale?>(value: Locale('en'), title: Text(l10n.en)),
-                  RadioListTile<Locale?>(value: Locale('zh'), title: Text(l10n.zh)),
-                ],
-              ),
+            Text(
+              l10n.lang,
+              style: Theme.of(context).textTheme.titleMedium,
             ),
-            // StreamBuilder(
-            //   stream: prefs.locale$,
-            //   initialData: prefs.locale,
-            //   builder: (context, snapshot) => ListTile(
-            //     leading: DropdownButton<Locale?>(
-            //       value: snapshot.data,
-            //       onChanged: (locale) => prefs.locale = locale,
-            //       items: [
-            //         .new(value: null, child: Text(l10n.systemDefault)),
-            //         .new(value: Locale('en'), child: Text(l10n.en)),
-            //         .new(value: Locale('zh'), child: Text(l10n.zh)),
-            //       ],
-            //     ),
-            //   ),
-            // ),
+            StreamBuilder(
+              stream: prefs.locale$,
+              builder: (context, snapshot) {
+                return RadioGroup<Locale?>(
+                  onChanged: (locale) => prefs.locale = locale,
+                  groupValue: prefs.locale,
+                  child: Column(
+                    children: [
+                      RadioListTile<Locale?>(
+                        value: null,
+                        title: Text(l10n.systemDefault),
+                      ),
+                      RadioListTile<Locale?>(
+                        value: Locale('en'),
+                        title: Text(l10n.en),
+                      ),
+                      RadioListTile<Locale?>(
+                        value: Locale('zh'),
+                        title: Text(l10n.zh),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
           ],
         ),
       ),
